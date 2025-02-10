fccbef713e8045dd866c21ee7d922fd7a2e453fe
DamianJaszewski <damianjaszewski@gmail.com>
2025-02-10 15:27:02 +0100

    add and configure integration test project

---
+++ WordFlowTest/Controllers/CategoriesControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using WordFlowServer;
+
+namespace WordFlowTest.Controllers
+{
+    public class CategoriesControllerTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public CategoriesControllerTest(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateDefaultClient();    
+        }
+
+        [Fact]
+        public async Task GetAll_EndpointsReturnSuccess()
+        {
+            var response = await _client.GetAsync("/Categories");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
