a0961a844e0b670b384a7c0c10b74592242d4bb7
DamianJaszewski <damianjaszewski@gmail.com>
2025-02-25 17:28:17 +0100

    Add test application factory

---
+++ WordFlowTest/Controllers/CardsContollerIntegrationTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using WordFlowServer;
+
+namespace WordFlowTest.Controllers
+{
+    public class CardsContollerIntegrationTest : IClassFixture<CustomWebApplicationFactory<Program>>, IDisposable
+    {
+        private readonly CustomWebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+        private readonly DataContext _context;
+        private readonly IServiceScope _scope;
+
+        public CardsContollerIntegrationTest(CustomWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _scope = _factory.Services.CreateScope();
+            _context = _scope.ServiceProvider.GetRequiredService<DataContext>();
+            _context.Database.EnsureCreated();
+            _client = factory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _scope.Dispose();
+        }
+
+        [Fact]
+        public async Task GetRandomTask_ReturnOk()
+        {
+            var response = await _client.GetAsync("/tag/populate");
+        }
+    }
+}
---
+++ WordFlowTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using WordFlowServer;
+
+namespace WordFlowTest
+{
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<DataContext>));
+
+                if (dbContextDescriptor != null)
+                {
+                    services.Remove(dbContextDescriptor);
+                }
+
+                var dbConnectionDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbConnection));
+
+                services.AddDbContext<DataContext>(options =>
+                {
+                    options.UseSqlServer(@"server = (localdb)\MSSQLLocalDB; database = wordFlowTest; trusted_connection = true");
+                });
+            });
+
+            builder.UseEnvironment("Development");
+        }
+    }
+}
