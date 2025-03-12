35c22694a28f6503a923633b3938f5134de4cf63
DamianJaszewski <damianjaszewski@gmail.com>
2025-03-12 15:02:27 +0100

    add Register User with integration test

---
+++ WordFlowServer/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WordFlowServer.Models;
+
+namespace WordFlowServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserDto userDto)
+        {
+            var user = new IdentityUser
+            {
+                UserName = userDto.UserName,
+                Email = userDto.Email,
+                PhoneNumber = userDto.PhoneNumber,
+            };
+
+            var result = await _userManager.CreateAsync(user, userDto.Password);
+
+            if (result.Succeeded)
+            {
+                return CreatedAtAction("Register", new { id = user.Id }, null);
+            }
+
+            // Zwróć błędy w przypadku niepowodzenia
+            return BadRequest(result.Errors.Select(e => e.Description));
+        }
+    }
+}
---
+++ WordFlowTest/Controllers/UsersControllerIntegrationTest.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.OpenApi.Models;
+using WordFlowServer;
+using WordFlowServer.Models;
+
+namespace WordFlowTest.Controllers
+{
+    public class UsersControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory<Program>>, IDisposable
+    {
+        private readonly CustomWebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+        private readonly DataContext _context;
+        private readonly IServiceScope _scope;
+
+        public UsersControllerIntegrationTest(CustomWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _scope = _factory.Services.CreateScope();
+            _context = _scope.ServiceProvider.GetRequiredService<DataContext>();
+            _context.Database.EnsureCreated();
+            _client = factory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _scope.Dispose();
+        }
+
+        [Fact]
+        public async Task RegisterUser_ReturnOk()
+        {
+            //Arrange: Tworzenie obiektu 
+            var userDto = new UserDto()
+            {
+                Email = "someaddress@fistaszki.com",
+                Password = "zaq1!QAZ",
+                PhoneNumber = "1234567890",
+                UserName = "username"
+            };
+
+            //Act
+            var response = await _client.PostAsJsonAsync("/api/Users/Register", userDto);
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(System.Net.HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task RegisterUser_WithBadPassword_ReturnBadRequest()
+        {
+            //Arrange: Tworzenie obiektu 
+            var userDto = new UserDto()
+            {
+                Email = "someaddress@fistaszki.com",
+                Password = "password",
+                PhoneNumber = "1234567890",
+                UserName = "username"
+            };
+
+            //Act
+            var response = await _client.PostAsJsonAsync("/api/Users/Register", userDto);
+
+            //Assert
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
